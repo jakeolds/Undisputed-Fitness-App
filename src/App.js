4d9870122c3f768df2d89cdf394b57b802f66071
jakeolds <jake.olds3@gmail.com>
2023-08-13 21:32:33 -0400

    Added animations, changed to dark theme, added shuffle

--- src/App.js
+++ src/App.js
@@ -2,55 +2,74 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [deck, setDeck] = useState(createDeck());
-  const [discardPile, setDiscardPile] = useState([]);
+  const [deck, setDeck] = useState(generateDeck());
+  const [shown, setShown] = useState([]);
 
-  const showNextCard = () => {
-    if (deck.length > 0) {
-      setDiscardPile([...discardPile, deck[0]]);
-      setDeck(deck.slice(1));
+  function generateDeck() {
+    const suits = ['♠', '♣', '♦', '♥'];
+    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    let deck = [];
+
+    for (let suit of suits) {
+      for (let value of values) {
+        deck.push({ value: value + suit, zIndex: 0 });
+      }
     }
+
+    return shuffle(deck);
+  }
+
+  function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
+
+  const handleCardClick = () => {
+    if (deck.length === 0) return;
+
+    let currentDeck = [...deck];
+    let card = currentDeck.pop();
+
+    card.zIndex = new Date().getTime();
+
+    setShown([...shown, card]);
+    setDeck(currentDeck);
   };
 
   return (
-    <div className="container mt-5">
-      <div className="deck-area" onClick={showNextCard}>
-        {deck.map((_, index) => (
-          <div key={index} className={`remaining-card ${index === 0 ? "top-card" : ""}`}></div>
+    <div className="container">
+      <div className="deck-area" onClick={handleCardClick}>
+        {deck.map(card => (
+          <div key={card.value} className="remaining-card">
+            <div className="card-back"></div>
+            <div className="card-front">
+              {card.value}
+            </div>
+          </div>
         ))}
       </div>
 
       <div className="discard-pile">
-        {discardPile.map((card, index) => (
-          <span key={index} className={`shown-card ${card.suit === "♥" || card.suit === "♦" ? "text-danger" : "text-dark"}`}>
-            {card.value}{card.suit}
-          </span>
+        {shown.map(card => (
+          <div key={card.value} className="shown-card" style={{ zIndex: card.zIndex }}>
+            <div className="card-back"></div>
+            <div className="card-front">
+              {card.value}
+            </div>
+          </div>
         ))}
       </div>
     </div>
   );
-}
-
-function createDeck() {
-  const suits = ['♠', '♣', '♥', '♦'];
-  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  let deck = [];
-  for (let suit of suits) {
-    for (let value of values) {
-      deck.push({ suit, value });
-    }
-  }
-  return shuffle(deck);
-}
-
-function shuffle(array) {
-  let currentIndex = array.length, randomIndex;
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-  }
-  return array;
 }
 
 export default App;
